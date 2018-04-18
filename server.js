3e984f1d0f11d533dc380d23051330a3ae66c314
Deep <deepti4597@gmail.com>
2018-04-18 18:33:14 +0530

    1st

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const api = require('./server/routes/api');
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
